425e376fb52bdc2d0909817571fbf3aebeaacc54
Mohammad Javad Imani <mjimani.ir@gmail.com>
2024-03-12 11:57:08 +0330

    feat: refactor send data api

--- app/Services/SendDataService.php
+++ app/Services/SendDataService.php
@@ -14,14 +14,21 @@ class SendDataService
             $data = $response->json();
 
             $last_id = $data['last_id'];
+            echo "\n last id: " . $last_id;
 
-            $new_data = LiveData::where('id', '>', $last_id)->limit(50)->get();
+            $new_data = LiveData::where('id', '>', $last_id)
+                ->orderBy('id', 'ASC')
+                ->limit(100)
+                ->get();
             if ($new_data) {
                 $res = Http::post(config('senddata.api_server_url') . '/api/save_data', ['data' => $new_data]);
-                if ($res){
-                    echo $res;
+                if ($res) {
+                    echo "\n new data saved: " . $res;
                     return 1;
                 }
+            } else {
+                echo "\n new data not found.";
+                return 1;
             }
         }
         return 0;
